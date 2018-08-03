271737b9e3e3125975ad421d17c854302184715e
zhouzhou <34015747+data-zhou@users.noreply.github.com>
2018-08-03 16:12:35 +0800

    Add files via upload

---
+++ shop/js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+  var swiper = new Swiper('.swiper-container', {
+      autoplay : 2000,
+      loop : true,
+        pagination: {
+          el: '.swiper-pagination',
+        },
+    });
+
+})
+
+
